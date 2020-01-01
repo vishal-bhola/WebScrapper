11a62e0882f23e48b545229e2ee3e3a63e66d97f
Vishal Bhola <38592928+VersatileVishal@users.noreply.github.com>
2020-01-01 23:14:11 +0530

    Update linkScrapping.py

--- linkScrapping.py
+++ linkScrapping.py
@@ -2,7 +2,7 @@ import requests
 import bs4
 
 res = requests.get('https://en.wikipedia.org/wiki/Machine_learning')
-print(type(res.text))
+#print(type(res.text))
 
 soup = bs4.BeautifulSoup(res.text,'lxml')
 
