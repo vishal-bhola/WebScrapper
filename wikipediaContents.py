fb1090c3b1aaa1a9f575e1e6346fac5a116eff33
Vishal Bhola <38592928+VersatileVishal@users.noreply.github.com>
2020-01-01 23:00:20 +0530

    Contents of Machine Learning Topics are Scrapped.
    All Contents of Machine Learning topics on Wikipedia inside content table are scrapped using BeautifulSoup & Requests.

---
+++ wikipediaContents.py
@@ -0,0 +1,13 @@
+import requests
+import bs4
+
+res = requests.get('https://en.wikipedia.org/wiki/Machine_learning')
+print(type(res))
+
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+print(type(soup))
+
+data = soup.select('.toc .toctext')  # selecting class
+
+for i in data:
+    print(i.getText())
